63ac4032d5b92cd99f429af934caa2acf0559892
Kushin0null <96753819+Kushin0null@users.noreply.github.com>
2023-05-07 22:29:49 +0100

    Update constants.ts

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -15,8 +15,8 @@ export const MENU_LINKS = [
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/chrismwilliams/astro-cactus",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
-	discord: "https://github.com/chrismwilliams/astro-cactus",
-	email: "me@example.com",
+	github: "test",
+	twitter: "test",
+	discord: "test",
+	email: "test@test.com",
 };
